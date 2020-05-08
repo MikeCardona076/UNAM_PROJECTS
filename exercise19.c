30f5d48c0e5f6bfa0e74629b86d7458fd68d9cd5
mikecardona076 <mikecardona076@gmail.com>
2020-05-08 14:44:28 -0700

    1.0

---
+++ exercise19.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+
+/*Que contenga una función llamada copiararreglo que reciba dos arreglos
+y el tamaño de los mismos (deben ser del mismo tamaño) y que copien en el 
+segundo arreglo el contenido del primero
+*/
+
+int main()
+{
+    int CopiarArreglo();
+
+    CopiarArreglo();
+
+    
+} 
+//Función para los arreglos
+print(int a[],int n)
+ { int i;
+    for(i=0; i<n; i++)
+    {
+         
+         
+    
+        printf("%d ",a[i]);
+         
+    }
+ 	
+}
+
+int CopiarArreglo(){
+    int a[1000],b[1000],i,n,c = 0;
+   
+    printf("Tamaño del primer arreglo : ");
+    scanf("%d", &n);
+    system("clear");
+
+    for(i=0; i<n; i++)
+    {   printf("Ingresar elemento [%d]: ",i+1);
+        scanf("%d", &a[i]);
+    }
+ 
+     for(i=0; i<n; i++)
+    {
+    	b[i]=a[i];
+     }
+ 
+  printf(" Primer arreglo   : ");
+    print(a,n);
+    
+    printf("\nSegundo arreglo : "); 
+    print(b,n);
+     return 0;
+
+}
---
+++ exercise20.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+/*
+Crear un arreglo de 100 números aleatorios del 1 al 1000. una vez creado,
+mostrar el contenido y después organizado de forma que estén juntos 
+los elementos pares e impares, después, volver a mostrar el arreglo
+*/
+
+int main(){
+    int numeroAleatorio[100]; //1 al 1000
+    srand (time(NULL));
+    for (int i=1; i<=100; i++) {
+        numeroAleatorio[i] = rand()%1000;
+        if( i % 2 == 0){
+            printf("Número Par %d vale: %d\n", i, numeroAleatorio[i]);
+        }
+        else{
+            printf("Número impar %d vale: %d\n", i, numeroAleatorio[i]);
+        }
+    }
+}
